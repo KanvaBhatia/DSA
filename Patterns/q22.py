8c81d4f94ba7eac9793100df72a566c5a39628b4
Kanva Bhatia <kanva.bhatia@gmail.com>
2023-10-20 19:10:01 -0400

    patterns q22

--- Patterns/q22.py
+++ Patterns/q22.py
@@ -2,4 +2,16 @@ out_num = 4
 rows = out_num * 2 - 1
 
 for i in range(rows):
-    
+    for j in range(rows):
+        print(out_num - min(i, j, (2*out_num - 2) - j, (2*out_num - 2) - i), end = "")
+    print()
+
+
+
+# 4444444
+# 4333334
+# 4322234
+# 4321234
+# 4322234
+# 4333334
+# 4444444
