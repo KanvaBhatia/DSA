335693af21a8124ac7395717f5c70841f69f8fd0
Kanva Bhatia <kanva.bhatia@gmail.com>
2023-12-26 22:53:53 +0530

    mistake

--- dsa_notcommited/Arrays/Medium/top-k-frequent-elements.py
+++
@@ -1,9 +0,0 @@
-# https://leetcode.com/problems/top-k-frequent-elements
-
-class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        hashmap = defaultdict(int)
-        for num in nums:
-            hashmap[num] += 1
-        sorted_keys = sorted(hashmap, key=lambda x: hashmap[x], reverse=True)
-        return sorted_keys[:k]
